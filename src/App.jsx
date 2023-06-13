34a5451d5d5cd320fb45bfe2cb47ab97d54762a5
Tamas Molnar <125677315+Tomocza@users.noreply.github.com>
2023-06-13 11:05:45 +0200

    create Dice component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 // use nextId() to generate a new unique ID when needed
-import nextId from "./generateId";
+import nextId from './generateId';
+import Dice from './components/Dice';
 
 function App() {
   return (
     <>
-      <p>Hello from the App!</p>
+      <Dice num={3} />
     </>
   );
 }
